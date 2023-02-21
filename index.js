bdaa45329b66a1879fce0efd340feedd5f4b34e5
Maksim Palagin <m.palagin91@gmail.com>
2023-02-21 15:01:48 +0700

    feat: implement class

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const GuessingGame = require('./src/guessing-game.js');
 
-window.game = new GuessingGame();
+//window.game = new GuessingGame();
+
+const game = new GuessingGame();
+game.setRange(0, 4048);
+
+let result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.lower();
+        result = game.guess();
+        game.greater();
+        result = game.guess();
+        game.greater();
+        result = game.guess();
+        game.lower();
+        result = game.guess();
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.temp;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+//        if (this.min >= this.max) {
+//            return this.min;
+//        }
+        this.temp = Math.floor((this.min + (this.max - this.min) / 2));
+//        console.log(this.temp);
+        return this.temp
     }
 
     lower() {
-
+        this.max = this.temp - 1;
+//        console.log(this.max);
     }
 
     greater() {
-
+        this.min = this.temp + 1;
+//        console.log(this.min);
     }
 }
 
